c480133879ba9c67d7b68414043427e0f862ae80
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-29 16:51:33 -0400

    adding state and event handling

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-
-const TODO_TASKS = ["Buy groceries", "Clean the house", "Walk the dog"];
+import React, { useState } from "react";
 
 const TodoList = () => {
-	const tasks = TODO_TASKS.map((task) => (
-		<li key={Math.random()} className="list-group-item">
-			{task}
-		</li>
-	));
+	const [todos, setTodos] = useState([
+		"Buy groceries",
+		"Clean the house",
+		"Walk the dog",
+	]);
+	const [selectedTodoIndex, setSelectedTodoIndex] = useState(-1);
+
+	const handleTodoClick = (index) => {
+		setSelectedTodoIndex(index);
+	};
 
 	return (
-		<>
-			<div className="card mx-auto mt-5" style={{ width: "18rem" }}>
-				<div className="card-body">
-					{!TODO_TASKS.length && <h2>You have no tasks on your list.</h2>}
-					<ul className="list-group">{tasks}</ul>
-				</div>
-			</div>
-		</>
+		<div className="w-50 mx-auto mt-5">
+			<h2>To-Do List</h2>
+			{!todos.length && (
+				<h3 className="mt-2 mb-2">You have no tasks on your list.</h3>
+			)}
+			<ul className="list-group">
+				{todos.map((todo, index) => (
+					<strong>
+						<li
+							className={`list-group-item ${
+								index === selectedTodoIndex ? "active" : ""
+							}`}
+							key={index}
+							onClick={() => handleTodoClick(index)}
+						>
+							{todo}
+						</li>
+					</strong>
+				))}
+			</ul>
+		</div>
 	);
 };
 
