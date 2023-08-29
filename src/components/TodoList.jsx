00046e11b18a4e473569f78bca2f43591dab111e
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-29 17:08:39 -0400

    input field for adding a new task

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,14 +7,22 @@ const TodoList = () => {
 		"Walk the dog",
 	]);
 	const [selectedTodoIndex, setSelectedTodoIndex] = useState(-1);
+	const [newTodo, setNewTodo] = useState("");
 
 	const handleTodoClick = (index) => {
 		setSelectedTodoIndex(index);
 	};
 
+	const handleAddTodo = () => {
+		if (newTodo.trim() !== "") {
+			setTodos([...todos, newTodo]);
+			setNewTodo("");
+		}
+	};
+
 	return (
 		<div className="w-50 mx-auto mt-5">
-			<h2>To-Do List</h2>
+			<h2>To Do List</h2>
 			{!todos.length && (
 				<h3 className="mt-2 mb-2">You have no tasks on your list.</h3>
 			)}
@@ -33,6 +41,23 @@ const TodoList = () => {
 					</strong>
 				))}
 			</ul>
+
+			<div className="input-group mt-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Add a new task"
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
+				/>
+				<button
+					className="btn btn-primary"
+					type="button"
+					onClick={handleAddTodo}
+				>
+					<strong>Button</strong>
+				</button>
+			</div>
 		</div>
 	);
 };
